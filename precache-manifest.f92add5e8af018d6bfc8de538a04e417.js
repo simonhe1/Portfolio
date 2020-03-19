b65acec864b9502fe4fa66553ddabe88e8da5fca
Simon <simonhe1@mail.com>
2020-03-19 12:40:46 -0400

    Updates

---
+++ precache-manifest.f92add5e8af018d6bfc8de538a04e417.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b4af5c10055d253c754975a1b398f5",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "a426178cafbd90992d4d",
+    "url": "/Portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "df6ee10b0be385602ee4",
+    "url": "/Portfolio/static/js/2.4dae4fdf.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Portfolio/static/js/2.4dae4fdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a426178cafbd90992d4d",
+    "url": "/Portfolio/static/js/main.61c3f982.chunk.js"
+  },
+  {
+    "revision": "c66aa5eafc120060b1dc",
+    "url": "/Portfolio/static/js/runtime-main.efb1fe8d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
